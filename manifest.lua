c919c0bb05650ec868a6d09379269b8c4fb571a3
KingoRodriguo <rodrigue.martignat@live.fr>
2025-09-18 14:12:59 +0200

    #

--- manifest.lua
+++ manifest.lua
@@ -1,5 +1,5 @@
 return {
-  version = "0.1.4",
+  version = "0.1.5",
   files = {
     ["usr/bin/telehub_node.lua"]      = "/usr/bin/telehub_node.lua",
     ["usr/lib/telehub/util.lua"]      = "/usr/lib/telehub/util.lua",
