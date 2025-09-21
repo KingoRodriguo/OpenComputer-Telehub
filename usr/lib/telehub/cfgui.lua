00727ae87971fafd5b662408be0e404077dc35b0
KingoRodriguo <rodrigue.martignat@live.fr>
2025-09-21 23:04:26 +0200

    Update cfgui.lua: change owner name placeholder from 'test' to 'test2'

--- usr/lib/telehub/cfgui.lua
+++ usr/lib/telehub/cfgui.lua
@@ -77,7 +77,7 @@ function M.run(cfg, saveFn)
     local lines = {
         -- Ligne 1 : Owner
         {
-            {type="text", value="[owner_name test]"},
+            {type="text", value="[owner_name test2]"},
             {type="pill", text="Edit", value=t.OWNER, bg=0x88FF88, fg=0x000000},
         },
         -- Ligne 2 : Destination
