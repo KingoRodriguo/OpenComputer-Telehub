60a15b09bff3b2eb22e9d6a5f3139a7f7a68bcd5
KingoRodriguo <rodrigue.martignat@live.fr>
2025-09-21 22:57:37 +0200

    Update manifest.lua: add telehub_uninstaller.lua to the file list

--- manifest.lua
+++ manifest.lua
@@ -1,15 +1,15 @@
 return {
   version = "0.1.6",
   files = {
-    ["usr/bin/telehub_node.lua"]      = "/usr/bin/telehub_node.lua",
-    ["usr/bin/telehub_update.lua"]    = "/usr/bin/telehub_update.lua",
-    ["usr/lib/telehub/util.lua"]      = "/usr/lib/telehub/util.lua",
-    ["usr/lib/telehub/config.lua"]    = "/usr/lib/telehub/config.lua",
-    ["usr/lib/telehub/detector.lua"]  = "/usr/lib/telehub/detector.lua",
-    ["usr/lib/telehub/access.lua"]    = "/usr/lib/telehub/access.lua",
-    ["usr/lib/telehub/redio.lua"]     = "/usr/lib/telehub/redio.lua",
-    ["usr/lib/telehub/ui.lua"]        = "/usr/lib/telehub/ui.lua",
-    ["usr/lib/telehub/cfgui.lua"]     = "/usr/lib/telehub/cfgui.lua",
-    ["usr/lib/telehub/update.lua"]    = "/usr/lib/telehub/update.lua",
+    ["usr/bin/telehub_node.lua"]         = "/usr/bin/telehub_node.lua",
+    ["usr/bin/telehub_update.lua"]       = "/usr/bin/telehub_update.lua",
+    ["usr/bin/telehub_uninstaller.lua"]  = "/usr/bin/telehub_uninstaller.lua",
+    ["usr/lib/telehub/util.lua"]         = "/usr/lib/telehub/util.lua",
+    ["usr/lib/telehub/config.lua"]       = "/usr/lib/telehub/config.lua",
+    ["usr/lib/telehub/detector.lua"]     = "/usr/lib/telehub/detector.lua",
+    ["usr/lib/telehub/access.lua"]       = "/usr/lib/telehub/access.lua",
+    ["usr/lib/telehub/redio.lua"]        = "/usr/lib/telehub/redio.lua",
+    ["usr/lib/telehub/ui.lua"]           = "/usr/lib/telehub/ui.lua",
+    ["usr/lib/telehub/cfgui.lua"]        = "/usr/lib/telehub/cfgui.lua",
+    ["usr/lib/telehub/update.lua"]       = "/usr/lib/telehub/update.lua",
   }
-}
